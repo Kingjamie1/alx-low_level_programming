513bec34394588780c48fc0bc732533de2210fc2
Kingjamie1 <chukwuebukajamie@gmail.com>
2023-02-20 12:45:55 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - prints all  combination of single digits
+ * main - prints all  combination of single digits numbers
+ *
  * Return: Always 0
  */
 int main(void)
 {
 	int n;
-	for (n = 48; n < 58; n++)
+
+	for (n = 0; n <= 9; n++)
 	{
-		putchar(n);
-	        if (n != 57)
-		{
-			putchar(',');
-			putchar(',');
-		}
+		putchar(n % 10) + '0');
+	        if (n == 9)
+		   continue;
+
+		putchar(',');
+		putchar(',');
+	}
+
 	 putchar('\n');
-         return (0);
-}	 
+
+	 return (0);
+} 
