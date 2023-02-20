de05be2ffe8a2c8f395d6a9ecc93870ad0d12ffd
Kingjamie1 <chukwuebukajamie@gmail.com>
2023-02-20 14:20:16 -0800

    100-intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
- *Return: Always 1
+ *main -  prints the statement and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *Return: Always 0
  */
 
 int main(void)
